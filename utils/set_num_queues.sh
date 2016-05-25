9a7e77da5052ab66998a29078e77961d60555cb4
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2016-05-25 23:40:09 +0200

    Really reboot

--- utils/set_num_queues.sh
+++ utils/set_num_queues.sh
@@ -12,22 +12,22 @@ UDIR=$(dirname "$0")
 [ -z "$1" ] || [ -z "$2" ] && usage
 
 if [ "$1" = "local" ]; then
+	echo "Configuring local system to use $2 queues"
 	if grep -qE '^hw.ix.num_queues=' /boot/loader.conf; then
 		sed -i '' -E 's/^(hw.ix.num_queues=).*/\1'"$2"'/' /boot/loader.conf
 	else 
 		echo "hw.ix.num_queues=$2" >> /boot/loader.conf
 	fi
-
-#	/sbin/reboot
-
+	/sbin/reboot
 else
+	echo "Configuring remote system to use $2 queues"
 	scp $UDIR/pincpus.sh $UDIR/set_num_queues.sh root@$1:/tmp
 	ssh root@$1 /tmp/set_num_queues.sh local $2
 	echo "Waiting 30s for reboot"
 	sleep 30
-	printf 'Waiting for %s to come back' "$1"
+	echo  "Waiting for $1 to come back"
 	while !	ssh -o ConnectTimeout=5 root@$1 /tmp/pincpus.sh 2>/dev/null; do
-		printf '.'
+		sleep 5
 	done
 	echo
 fi
