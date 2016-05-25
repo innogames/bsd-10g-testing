bc533e34c0cabd23b45e76078d8e191f8dfae4e3
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2016-05-26 00:17:50 +0200

    Let this test run up to 2.5Mpps with steps of 25kpps

--- perftests/maxpps_route.sh
+++ perftests/maxpps_route.sh
@@ -4,13 +4,13 @@
 
 . $WDIR/config
 
-STEP=100
+STEP=25
 
 for QUEUES in `seq 1 6`; do
 	TXT_LOG=$(logname ${QUEUES}_txt)
 	IPGEN_LOG=$(logname ${QUEUES}_ipgen)
 	$UDIR/set_num_queues.sh $GW_TX $QUEUES | tee -a $TXT_LOG
-	$UDIR/gen_ipgen_script.sh $STEP 10000 > $TMPSCRIPT
+	$UDIR/gen_ipgen_script.sh $STEP 2500 > $TMPSCRIPT
 	$UDIR/ipgen.sh $GW_TX $GW_RX 1000 $TMPSCRIPT $IPGEN_LOG
 done
 
