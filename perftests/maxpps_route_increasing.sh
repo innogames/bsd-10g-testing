01313dcc651b472b07b2420a89b6da416079005b
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2016-05-26 16:15:28 +0200

    Use separate script for static and increase traffic

--- perftests/maxpps_route_increasing.sh
+++ perftests/maxpps_route_increasing.sh
@@ -4,13 +4,14 @@
 
 . $WDIR/config
 
-STEP=25
+STEP=100
+MAX=5000
 
-for QUEUES in `seq 1 6`; do
+for QUEUES in `seq 3 6`; do
 	TXT_LOG=$(logname ${QUEUES}_txt)
 	IPGEN_LOG=$(logname ${QUEUES}_ipgen)
 	$UDIR/set_num_queues.sh $GW_TX $QUEUES | tee -a $TXT_LOG
-	$UDIR/gen_ipgen_script.sh $STEP 2500 > $TMPSCRIPT
+	$UDIR/gen_ipgen_script.sh $STEP $MAX > $TMPSCRIPT
 	$UDIR/ipgen.sh $GW_TX $GW_RX 1000 $TMPSCRIPT $IPGEN_LOG
 done
 
---
+++ perftests/maxpps_route_static.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+. $(dirname "$0")/../lib/init.sh
+
+. $WDIR/config
+
+STEP=100
+MAX=5000
+
+for QUEUES in `seq 3 6`; do
+	TXT_LOG=$(logname ${QUEUES}_txt)
+	IPGEN_LOG=$(logname ${QUEUES}_ipgen)
+	$UDIR/set_num_queues.sh $GW_TX $QUEUES | tee -a $TXT_LOG
+	echo "100 tx1 46 3000000" > $TMPSCRIPT
+	$UDIR/ipgen.sh $GW_TX $GW_RX 1000 $TMPSCRIPT $IPGEN_LOG
+done
+
