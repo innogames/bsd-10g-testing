8a759809e1927f44979bcf8460b2c5caa1e660a9
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2016-05-26 09:17:53 +0200

    Pin CPUs starting from 0

--- utils/pincpus.sh
+++ utils/pincpus.sh
@@ -6,9 +6,10 @@ for iface in $(ifconfig -l); do
 			echo "Interface $iface:"
 			cpu=0
 			for irq in $(vmstat -i | grep -E "$iface"':q[0-9]' | sed -E 's/irq([0-9]+).*/\1/'); do
-			[ -z "$1" ] && cpu=$(($cpu+1)) || cpu=$1
+			[ -n "$1" ] && cpu=$1
 		        echo "irq $irq to cpu $cpu";
 		        cpuset -x $irq -l $cpu;
+			cpu=$(($cpu+1))
 		done
 		;;
 	esac
