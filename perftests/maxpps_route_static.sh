cd767e9f6b8bedfdbb87d73e14b219fadc193dba
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2016-05-28 00:58:51 +0200

    Fix amount of tested queues

--- perftests/maxpps_route_static.sh
+++ perftests/maxpps_route_static.sh
@@ -7,7 +7,7 @@
 STEP=100
 MAX=5000
 
-for QUEUES in `seq 3 6`; do
+for QUEUES in `seq 1 12`; do
 	TXT_LOG=$(logname ${QUEUES}_txt)
 	IPGEN_LOG=$(logname ${QUEUES}_ipgen)
 	$UDIR/set_num_queues.sh $GW_TX $QUEUES | tee -a $TXT_LOG
