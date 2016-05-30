05b2029a860c26f5fcb42796858f906d2800bf08
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2016-05-30 13:54:47 +0200

    Make smaller steps, check 6 queues

--- perftests/maxpps_route_increasing.sh
+++ perftests/maxpps_route_increasing.sh
@@ -4,10 +4,10 @@
 
 . $WDIR/config
 
-STEP=100
-MAX=5000
+STEP=25
+MAX=3000
 
-for QUEUES in `seq 3 6`; do
+for QUEUES in `seq 1 6`; do
 	TXT_LOG=$(logname ${QUEUES}_txt)
 	IPGEN_LOG=$(logname ${QUEUES}_ipgen)
 	$UDIR/set_num_queues.sh $GW_TX $QUEUES | tee -a $TXT_LOG
