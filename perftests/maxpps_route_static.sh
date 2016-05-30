dc2113c7b8fda60e69efb1650f056eac71092595
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2016-05-30 15:17:17 +0200

    Add some logging so that we can find graphs in graphite later on

--- perftests/maxpps_route_static.sh
+++ perftests/maxpps_route_static.sh
@@ -7,6 +7,8 @@
 for QUEUES in `seq 1 12`; do
 	TXT_LOG=$(logname ${QUEUES}_txt)
 	IPGEN_LOG=$(logname ${QUEUES}_ipgen)
+	date | tee -a $TXT_LOG
+	echo "Running test on $QUEUES" | tee -a $TXT_LOG
 	$UDIR/set_num_queues.sh $GW_TX $QUEUES | tee -a $TXT_LOG
 	echo "180 tx1 46 3000000" > $TMPSCRIPT
 	$UDIR/ipgen.sh $GW_TX $GW_RX 1000 $TMPSCRIPT $IPGEN_LOG
