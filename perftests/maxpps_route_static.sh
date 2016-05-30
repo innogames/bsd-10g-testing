19dfccd737b8bbf9ba6365ed6c7bd70c1386e932
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2016-05-30 13:53:35 +0200

    No need for step definition in step-less test. Test for 3 minutes for better graphs.

--- perftests/maxpps_route_static.sh
+++ perftests/maxpps_route_static.sh
@@ -4,14 +4,11 @@
 
 . $WDIR/config
 
-STEP=100
-MAX=5000
-
 for QUEUES in `seq 1 12`; do
 	TXT_LOG=$(logname ${QUEUES}_txt)
 	IPGEN_LOG=$(logname ${QUEUES}_ipgen)
 	$UDIR/set_num_queues.sh $GW_TX $QUEUES | tee -a $TXT_LOG
-	echo "100 tx1 46 3000000" > $TMPSCRIPT
+	echo "180 tx1 46 3000000" > $TMPSCRIPT
 	$UDIR/ipgen.sh $GW_TX $GW_RX 1000 $TMPSCRIPT $IPGEN_LOG
 done
 
