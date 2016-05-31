5b0f86985285cadefb3d1d5ff5e0c01aae758604
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2016-05-31 12:55:08 +0200

    Make flow number easier configurable

--- perftests/maxpps_route_static.sh
+++ perftests/maxpps_route_static.sh
@@ -4,6 +4,8 @@
 
 . $WDIR/config
 
+FLOWS=1000
+
 for QUEUES in `seq 1 12`; do
 	TXT_LOG=$(logname ${QUEUES}_txt)
 	IPGEN_LOG=$(logname ${QUEUES}_ipgen)
@@ -11,6 +13,6 @@ for QUEUES in `seq 1 12`; do
 	echo "Running test on $QUEUES" | tee -a $TXT_LOG
 	$UDIR/set_num_queues.sh $GW_TX $QUEUES | tee -a $TXT_LOG
 	echo "180 tx1 46 3000000" > $TMPSCRIPT
-	$UDIR/ipgen.sh $GW_TX $GW_RX 1000 $TMPSCRIPT $IPGEN_LOG
+	$UDIR/ipgen.sh $GW_TX $GW_RX $FLOWS $TMPSCRIPT $IPGEN_LOG
 done
 
